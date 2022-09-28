1ae801973406edbb9c8c9b6702b399af86650d47
yangwei <12345678>
2022-09-28 16:10:52 +0800

    available

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-module.exports = require("./lib");
+import cusTable from './lib/components/custable.vue'
+import searchForm from './lib/components/searchForm.vue'
+import './lib/assets/one_common.css'
+
+const components = [cusTable, searchForm];
+
+const install = function (Vue) {
+    components.forEach((component) => {
+        console.log(component.name);
+        Vue.component(component.name, component);
+    });
+};
+
+export { cusTable, searchForm };
